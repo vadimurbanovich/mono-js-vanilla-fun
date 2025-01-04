eebd7d5ac9e0b1f4689383bab0602051fe3a8999
Vadim Urbanovich <vadymurbanovych@gmail.com>
2025-01-04 11:37:30 +0100

    toggle-fix

--- toggle/main.js
+++ toggle/main.js
@@ -1,22 +1,22 @@
 const inputEl = document.querySelector(".dark-mode");
 const pageEl = document.querySelector(".page");
 
-inputEl.checked = JSON.parse(localStorage.getItem('mode'));
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
 updateBody();
 
 function updateBody() {
   if (inputEl.checked) {
-    pageEl.style.background = 'black';
+    pageEl.style.background = "#003049";
   } else {
-    pageEl.style.background = "white";
+    pageEl.style.background = "#eae2b7";
   }
 }
 
-inputEl.addEventListener('input', ()=>{
+inputEl.addEventListener("input", () => {
   updateBody();
   updateLocalStorage();
-})
+});
 
 function updateLocalStorage() {
   localStorage.setItem("mode", JSON.stringify(inputEl.checked));
