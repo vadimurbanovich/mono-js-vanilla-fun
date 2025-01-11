78660923e1f2467a4c533847cf59eb03a5a5e7f8
Vadim Urbanovich <vadymurbanovych@gmail.com>
2025-01-11 18:48:22 +0100

    search-bar

---
+++ search-bar/main.js
@@ -0,0 +1,6 @@
+const searchBarContainerEl = document.querySelector(".search-bar__container");
+const magnifierEl = document.querySelector(".search-bar__magnifier");
+
+magnifierEl.addEventListener("click", () => {
+  searchBarContainerEl.classList.toggle('active')
+});
